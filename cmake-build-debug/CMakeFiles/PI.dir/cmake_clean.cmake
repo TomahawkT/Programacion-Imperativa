c79a2a9664c40d78b1e02e8bce4ceb55e49bc26d
TomahawkT <tommy40@me.com>
2021-08-02 23:50:08 -0300

    initial import

---
+++ cmake-build-debug/CMakeFiles/PI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PI.dir/main.c.o"
+  "PI"
+  "PI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
