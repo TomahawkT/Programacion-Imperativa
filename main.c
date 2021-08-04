0b2c41fd9505c45a9746d1f20e9537981de61e4d
TomahawkT <tommy40@me.com>
2021-08-04 10:13:32 -0300

    Cambio para testear si CLion usa signed o unsigned char como default

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int i;
-    for (i = 0; i < 10; i++){
-        printf("%d", i);
-    };
-    return 0;
+    char a,b;
+    a = 100;
+    b = a + a;
+    printf("%d", b);
 }
